5977633f81d1663cc32692a9443f3a7226222a89
Kyle <lookerkj@protonmail.com>
2018-08-14 11:51:29 -0700

    added a touch more detail to the app info

--- src/components/AppInfo.js
+++ src/components/AppInfo.js
@@ -2,10 +2,11 @@ import React from 'react';
 export default function AppInfo() {
 	return(
 		<section className="App-info">
-          <p className="App-about">THIS IS WHAT THE APP DOES</p>
-          <p className="App-intro">
-            To get started, click login or register.
-          </p>
-        </section>
+      <header role="banner">
+        <h1>Flash till you crash</h1>
+        <h2>A custopmizable flash card deck able to inform the user areas of strength and weekness
+        </h2>
+      </header>
+    </section>
 	);
 }
